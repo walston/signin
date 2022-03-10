d0cdada59c8ee09d9d23ca20960ba1cf1decfd34
streetlightkids <ltj_hq@hotmail.com>
2022-03-09 17:37:09 -0800

    CreateUser form

--- src/routes/CreateUser.tsx
+++ src/routes/CreateUser.tsx
@@ -1,6 +1,29 @@
 import React from "react";
 import Form from "../components/Form";
+import Button from "../components/Button";
 
-export default function CreateUser({ children }) {
-  return <Form>{children}</Form>;
+export default function CreateUser() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  return (
+    <Form>
+      <label htmlFor="userName">Username</label>{" "}
+      <input
+        type="text"
+        value={username}
+        onChange={(event) => setUsername(event?.target.value)}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event?.target.value)}
+      />
+      <div>
+        <Button color="300" style={{ display: "block" }}>
+          Create User
+        </Button>
+      </div>
+    </Form>
+  );
 }
