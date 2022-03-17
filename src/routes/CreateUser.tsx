ea6280f29e5534059634d3fd2e10d3a6bbd9ea2d
streetlightkids <ltj_hq@hotmail.com>
2022-03-16 17:23:24 -0700

    CreateUser page complete
    -imported useNavigate to reroute to a new page if successful
    - create async to handle creating a new user id page
    -onClick create a random user ID and page then navigate to the users page

--- src/routes/CreateUser.tsx
+++ src/routes/CreateUser.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import Form from "../components/Form";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
+
+async function sendCreateUserRequest({ id, username, password }) {
+  const body = JSON.stringify({ username, password });
+  console.log({ id, username, password });
+  return fetch(`http://localhost:3001/users/${id}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body,
+  }).then((res) => {
+    if (!res.ok) {
+      throw Error("Nahhhh");
+    }
+  });
+}
+
+function createUserID() {
+  const userID = Math.floor(Math.random() * 26000);
+  return userID;
+}
 
 export default function CreateUser() {
+  const navigate = useNavigate();
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   return (
-    <Form>
+    <Form onSubmit={(event) => event.preventDefault()}>
       <label htmlFor="userName">Username</label>{" "}
       <input
         type="text"
@@ -20,7 +41,17 @@ export default function CreateUser() {
         onChange={(event) => setPassword(event?.target.value)}
       />
       <div>
-        <Button color="300" style={{ display: "block" }}>
+        <Button
+          color="300"
+          style={{ display: "block" }}
+          onClick={() => {
+            sendCreateUserRequest({
+              id: createUserID(),
+              username: username,
+              password: password,
+            }).then(() => navigate("/user"));
+          }}
+        >
           Create User
         </Button>
       </div>
