bac0d81ec0f292f15e0390ab181558e7ef51e473
streetlightkids <ltj_hq@hotmail.com>
2022-03-17 15:58:12 -0700

    connect MainUser to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateUser from "./CreateUser";
+import MainUser from "./MainUser";
 import Home from "./Home";
 
 export default function AppRoutes() {
@@ -9,7 +10,7 @@ export default function AppRoutes() {
         <Route index element={<Home />} />
         <Route path="create/" element={<CreateUser />} />
         <Route path="user/">
-          <Route index element="Main User" />
+          <Route index element={<MainUser />} />
           <Route path="edit/" element="User edit" />
           <Route path="delete/" element="Delete user" />
         </Route>
