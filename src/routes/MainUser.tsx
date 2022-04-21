80c2786d3aab65145c8a89e6beab57902b7d1cbf
streetlightkids <ltj_hq@hotmail.com>
2022-04-20 17:51:33 -0700

    Update MainUser()
    -removed unnecessary input tags
    -added new image url

--- src/routes/MainUser.tsx
+++ src/routes/MainUser.tsx
@@ -9,14 +9,8 @@ export default function MainUser() {
       <div id="avatar"></div>
       <h1 id="hello">Hello, DYNAMIC NAME.</h1>
       <Form>
-        <label>First name</label>
-        <input type="text" value="DYANAMIC First NAME" />
-        <label>Last name</label>
-        <input type="text" value="DYANAMIC LAST NAME" />
-        <label>Email</label>
-        <input type="email" value="DYANAMIC EMAIL" />
         <label>Username</label>
-        <input type="text" value="DYANAMIC USER NAME" />
+        <input type="text" placeholder="DYANAMIC USER NAME" />
         <label>Password</label>
         <input type="password" />
       </Form>
