eec3749a44da2ede1868f7af84307c7ad7322c75
streetlightkids <ltj_hq@hotmail.com>
2022-05-18 17:34:01 -0700

    imports and uses auth file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import users from "./users.js";
+import auth from "./auth.js";
 import cors from "cors";
 
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors(), express.static("build"));
 
+app.use("/auth", auth);
 app.use("/users", users);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
