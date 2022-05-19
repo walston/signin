993e87c5eb71483ab3cc3c5a5d6e1924850ad1bd
streetlightkids <ltj_hq@hotmail.com>
2022-05-18 17:33:29 -0700

    removes type from component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import "./Button.css";
 
 export default function Button({ children, color = "", ...props }) {
   return (
-    <button type="submit" style={{ "--color": color } as any} {...props}>
+    <button style={{ "--color": color } as any} {...props}>
       {children}
     </button>
   );
