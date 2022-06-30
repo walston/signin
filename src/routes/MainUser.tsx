cc21b7b700aab89566b06d6f485ce2d9d5cebe70
streetlightkids <ltj_hq@hotmail.com>
2022-06-30 07:00:08 -0700

    Adds functionality to the update user button onClick

--- src/routes/MainUser.tsx
+++ src/routes/MainUser.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getUser } from "../api/user";
+import { getUser, sendUpdateUserRequest } from "../api/user";
 import Button from "../components/Button";
 import Form from "../components/Form";
 import "./MainUser.css";
 
 type User = { userID: number; username: string; isLoading: false };
 type UserLoading = { isLoading: true };
+
 export default function MainUser() {
   const { id } = useParams();
-  console.log(id);
+
   const [user, setUser] = React.useState<User | UserLoading>({
     isLoading: true,
   });
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState("");
+
   useEffect(() => {
     getUser(id).then((user) => {
       setUser({ ...user, isLoading: false });
@@ -31,15 +36,49 @@ export default function MainUser() {
       <h1 id="hello">Hello, {user.username}.</h1>
       <Form>
         <label>Username</label>
-        <input type="text" placeholder={user.username} />
+        <input
+          type="text"
+          placeholder={user.username}
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
         <label>Password</label>
-        <input type="password" placeholder="PASSWORD" />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
         <label>Email</label>
-        <input type="email" placeholder="DYANAMIC EMAIL" />
+        <input
+          type="email"
+          placeholder="DYANAMIC EMAIL"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
       </Form>
       <div id="btnCont">
         {" "}
-        <Button color="1000">Update User</Button>
+        <Button
+          onClick={() => {
+            sendUpdateUserRequest(id, { username, password, email })
+              .then(console.log)
+              /**
+               * @TODO
+               * Present user with error UI and success UI
+               * */
+              .catch(console.error);
+          }}
+          //add error handling to sendUpdateUserRequest()
+          color="1000"
+        >
+          Update User
+        </Button>
         <Button color="3">Delete User</Button>
       </div>
     </div>
