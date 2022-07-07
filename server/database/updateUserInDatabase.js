fb01c58d81f31925fba319f0e56611e9ad3cfa52
streetlightkids <ltj_hq@hotmail.com>
2022-07-06 18:17:23 -0700

    Refactors updateUserInDatabase() validation

--- server/database/updateUserInDatabase.js
+++ server/database/updateUserInDatabase.js
@@ -4,13 +4,40 @@ import EmailValidator from "email-validator";
 import { passwordSecurity } from "./passwordSecurity.js";
 
 export async function updateUserInDatabase(id, { username, pass, email }) {
-  const alphaCheck = /[a-zA-Z]/;
-  if (
-    typeof username === "string" &&
-    username !== "" &&
-    alphaCheck.test(username)
-  ) {
-    console.log("Username s'good");
+  const hasUsername = typeof username === "string" && username !== "";
+  const hasPassword = typeof pass === "string" && pass !== "";
+  const hasEmail = typeof email === "string" && email !== "";
+
+  if (hasUsername) {
+    const alphaCheck = /[a-zA-Z]/;
+    if (alphaCheck.test(username)) {
+      console.log("Username s'good");
+    } else {
+      throw Error(
+        "UserInputError: username doesn't match expected format /[a-zA-Z]/"
+      );
+    }
+  }
+
+  if (hasPassword) {
+    const passCheck = zxcvbn(pass);
+    if (passCheck.score >= 2) {
+      console.log("It's a solid password");
+    } else {
+      throw Error("UserInputError: " + passCheck.feedback.warning);
+    }
+  }
+
+  if (hasEmail) {
+    const isEmailValid = EmailValidator.validate(email);
+    if (isEmailValid) {
+      console.log("Email s'good");
+    } else {
+      console.log("Invalid email input");
+    }
+  }
+
+  if (hasUsername) {
     const updateUsernameStatement = db.prepare(
       `UPDATE user
         SET user_name = ? WHERE user_id = ?`
@@ -24,57 +51,40 @@ export async function updateUserInDatabase(id, { username, pass, email }) {
         }
       });
     });
-  } else {
-    throw Error(
-      "UserInputError: username doesn't match expected format /[a-zA-Z]/"
-    );
   }
 
-  if (typeof pass === "string" && pass !== "") {
-    const passCheck = zxcvbn(pass);
-
-    if (passCheck.score >= 2) {
-      console.log("It's a solid password");
-      const passSecurity = await passwordSecurity(pass);
-      const updateSaltedHashStatement = db.prepare(
-        `UPDATE user
+  if (hasPassword) {
+    const passSecurity = await passwordSecurity(pass);
+    const updateSaltedHashStatement = db.prepare(
+      `UPDATE user
         SET hash = ? AND salt = ? WHERE user_id = ?`
-      );
-      await new Promise((res, rej) => {
-        updateSaltedHashStatement
-          .run(passSecurity.hash, passSecurity.salt, id)
-          .finalize((error) => {
-            if (error) {
-              rej(error);
-            } else {
-              res();
-            }
-          });
-      });
-    } else {
-      throw Error("UserInputError: " + passCheck.feedback.warning);
-    }
-  }
-
-  if (typeof email === "string" && email !== "") {
-    const isEmailValid = EmailValidator.validate(email);
-    if (isEmailValid) {
-      console.log("Email s'good");
-      const updateEmailStatement = db.prepare(
-        `UPDATE user
-        SET user_email = ? WHERE user_id = ?`
-      );
-      await new Promise((res, rej) => {
-        updateEmailStatement.run(email, id).finalize((error) => {
+    );
+    await new Promise((res, rej) => {
+      updateSaltedHashStatement
+        .run(passSecurity.hash, passSecurity.salt, id)
+        .finalize((error) => {
           if (error) {
             rej(error);
           } else {
             res();
           }
         });
+    });
+  }
+
+  if (hasEmail) {
+    const updateEmailStatement = db.prepare(
+      `UPDATE user
+        SET user_email = ? WHERE user_id = ?`
+    );
+    await new Promise((res, rej) => {
+      updateEmailStatement.run(email, id).finalize((error) => {
+        if (error) {
+          rej(error);
+        } else {
+          res();
+        }
       });
-    } else {
-      console.log("Invalid email input");
-    }
+    });
   }
 }
