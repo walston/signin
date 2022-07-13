1ee5da7bd274f23960e78db548a36e90b8c20630
streetlightkids <ltj_hq@hotmail.com>
2022-07-13 16:48:25 -0700

    Refactors code to support input with type submit as well as button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,11 @@
 import "./Button.css";
 
 export default function Button({ children, color = "", ...props }) {
+  if (props.type === "submit") {
+    return (
+      <input value={children} style={{ "--color": color } as any} {...props} />
+    );
+  }
   return (
     <button style={{ "--color": color } as any} {...props}>
       {children}
